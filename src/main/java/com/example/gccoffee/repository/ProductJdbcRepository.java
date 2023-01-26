23c3cf1b9018270f92e4a7e4b783aedc406b3290
ykgod98 <kyum0401@gmail.com>
2023-01-26 17:00:11 +0900

    insert

---
+++ src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -0,0 +1,83 @@
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+import static com.example.gccoffee.JdbcUtils.*;
+@Repository
+public class ProductJdbcRepository implements ProductRepository{
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    private static final RowMapper<Product> productRowMapper = (resultSet, i) -> {
+        var productId = toUUID(resultSet.getBytes("product_id"));
+        var productName = resultSet.getString("product_name");
+        var category = Category.valueOf(resultSet.getString("category"));
+        var price = resultSet.getLong("price");
+        var description = resultSet.getString("description");
+        var createdAt = toLocalDateTime(resultSet.getTimestamp("created_at"));
+        var updatedAt = toLocalDateTime(resultSet.getTimestamp("updated_at"));
+
+        return new Product(productId, productName, category, price, description, createdAt, updatedAt);
+    };
+
+    private Map<String, Object> toParamMap(Product product) {
+        var paramMap = new HashMap<String, Object>();
+        paramMap.put("productId", product.getProductId().toString().getBytes());
+        paramMap.put("productName", product.getProductName().toString());
+        paramMap.put("category", product.getCategory().toString());
+        paramMap.put("price", product.getPrice());
+        paramMap.put("description", product.getDescription());
+        paramMap.put("createdAt", product.getCreatedAt());
+        paramMap.put("updatedAt", product.getUpdatedAt());
+        return paramMap;
+    }
+
+    public ProductJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return jdbcTemplate.query("select * from products", productRowMapper);
+    }
+
+    @Override
+    public Product insert(Product product) {
+        var update= jdbcTemplate.update("INSERT INTO products(product_id, product_name, category, price, description, created_at, updated_at) "+
+                "VALUES(UNHEX(REPLACE(:productId, '-', '')),:productName, :category, :price, :description, :createdAt, :updatedAt)",toParamMap(product));
+        if(update!=1){
+            throw new RuntimeException("Noting was inserted");
+        }
+        return product;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+
+    @Override
+    public Optional<Product> findById(UUID productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> findByName(String productName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Product> findByCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
---
+++ src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.ScriptResolver;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.distribution.Version;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
+
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static com.wix.mysql.distribution.Version.v5_7_latest;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+class ProductJdbcRepositoryTest {
+
+
+    static EmbeddedMysql embeddedMysql;
+
+
+    @BeforeAll
+    static void setup() {
+        var config = aMysqldConfig(v5_7_latest)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+        embeddedMysql = anEmbeddedMysql(config)
+                .addSchema("test-order_mgmt", ScriptResolver.classPathScript("schema.sql"))
+                .start();
+    }
+    @AfterAll
+    static void cleanup() {
+        embeddedMysql.stop();
+    }
+    @Autowired
+    ProductRepository repository;
+
+
+    private Product newProduct = new Product(UUID.randomUUID(), "new-product", Category.COFFEE_BEAN_PACKAGE, 1000L);
+
+    @Test
+    @Order(1)
+    @DisplayName("상품을 추가할 수 있다.")
+    void testInsert() {
+        repository.insert(newProduct);
+        var all = repository.findAll();
+        assertThat(all.isEmpty(), is(false));
+    }
+}
